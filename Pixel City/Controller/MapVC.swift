25e2e756853af513d45c49a120658d585e963544
Clement M <massardc@users.noreply.github.com>
2017-11-14 23:57:58 +0100

    Initialized Flickr API URL with key

--- Pixel City/Controller/MapVC.swift
+++ Pixel City/Controller/MapVC.swift
@@ -145,7 +145,7 @@ extension MapVC: MKMapViewDelegate {
         
         let annotation = DroppablePin(coordinate: touchCoordinate, identifier: "droppablePin")
         mapView.addAnnotation(annotation)
-        
+                
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(touchCoordinate, regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
     }
--- Pixel City/Utilities/Constants.swift
+++ Pixel City/Utilities/Constants.swift
@@ -2,8 +2,15 @@
 //  Constants.swift
 //  Pixel City
 //
-//  Created by School on 14/11/2017.
+//  Created by ClementM on 14/11/2017.
 //  Copyright © 2017 ClementM. All rights reserved.
 //
 
 import Foundation
+
+func flickrUrl (forApiKey key: String, withAnnotation annotation: DroppablePin, andNumberOfPhotos number: Int) -> String {
+    let url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(key)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
+    
+    return url
+}
+
